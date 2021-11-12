43439333819869a282da6fb9eaa84a4dd8fa2ffb
Saurav Dahal <dahal.saurav123@gmail.com>
2021-11-12 01:33:17 +0100

    add LinkVisit: entity, Repository, migrations, manyToOne relation with Link, create LinkVisit while resolving link

--- src/src/Entity/Link.php
+++ src/src/Entity/Link.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\LinkRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Link
      * @ORM\Column(type="string", length=10, unique=true)
      */
     private string $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\LinkVisit", mappedBy="link")
+     */
+    private Collection $linkVisits;
+
+    public function __construct()
+    {
+        $this->linkVisits = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -54,5 +66,13 @@ class Link
         $this->slug = $slug;
 
         return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLinkVisitCounts(): int
+    {
+        return $this->linkVisits->count();
     }
 }
